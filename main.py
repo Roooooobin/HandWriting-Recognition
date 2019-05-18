8b2bf0f0bc8f4ce8d181f7f12a1e9c98a30fe5c3
Roooooobin <35558127+Roooooobin@users.noreply.github.com>
2019-05-18 15:00:06 +0800

    well structured

---
+++ main.py
@@ -0,0 +1,4 @@
+from fitting import fit
+
+if __name__ == "__main__":
+    fit()
--- modeling.py
+++ modeling.py
@@ -1,4 +1,4 @@
-from H
+from preparing import get_data
 from keras import models
 from keras import layers
 import numpy as np
@@ -16,20 +16,3 @@ def model_conv():
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
     return model
 
-
-x_train, x_test, y_train, y_test = get_data()
-# 定义模型
-model = model_conv()
-model.summary()
-his = model.fit(x_train, y_train, epochs=5, batch_size=256, validation_split=0.1)
-
-# 看下测试集的损失值和准确率
-loss, acc = model.evaluate(x_test, y_test)
-print('loss {}, acc {}'.format(loss, acc))
-model.save("my_mnist_model.h5")
-'''
-    output:
-            (60000, 28, 28, 1) (60000, 10)
-            loss 0.02437469101352144, acc 0.9927
-    测试集结果是99.27%，非常不错的模型
-'''
